c3e26d6863d6c4b16815991f6e8a76ec8fbc4075
liaoxin <lx13337339627@163.com>
2021-12-02 08:46:01 +0800

    chora: add commitlint to force commit style

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    // other configs ...
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+    'prettier/unicorn',
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 13,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "@typescript-eslint"
+  ],
+  "rules": {
+
+    'import/extensions': [
+      ERROR,
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+        json: 'never',
+        js: 'never',
+      },
+    ],
+  },
+  "settings": {
+    'import/resolver': {
+      node: {
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+      },
+    },
+  },
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # react-ts-projectOne
 第一个react项目，react+ typescript
+
+react目录结构
+------ react-ts-projectOne
+-----------  .gitignore   //项目进行 git 提交时所需要忽略掉的文件或文件夹
+-----------  .npmrc       //设置为淘宝镜像
+-----------  LICENSE     //开源协议
+-----------  package.json    //记录项目的配置信息
